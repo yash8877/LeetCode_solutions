16c8fb7525f5954535d5426b8ee1d1b54bdb509e
Yash Raj <yash524767@gmail.com>
2024-07-13 21:53:31 +0530

    121

---
+++ Leetcode121.java
@@ -0,0 +1,23 @@
+public class Leetcode121 {
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int profit = 0;
+        int min = prices[0];
+        for (int i = 1; i < n; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            }
+            else{
+                int diff = prices[i] - min;
+                if (diff > profit) {
+                    profit = diff;
+                }
+            }
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] nums = {7,1,5,3,6,4};
+        System.out.println(maxProfit(nums));
+    }
+}
